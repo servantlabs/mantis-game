f3ac64e583676edf134a85dcbaa860f6b61269af
Aaron Bray <aaron@servantlabs.com>
2022-08-25 19:03:44 -0500

    First card story created with background colors

---
+++ stories/Card.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './card.css';
+
+/**
+ * Primary UI component for user interaction
+ */
+export const Card = ({ dealt, backColor1, backColor2, backColor3, frontColor, ...props }) => {
+  const mode = dealt ? 'dealt' : 'inDeck';
+  const cardBackground = {
+    background: `linear-gradient(160deg, ` + backColor1 + ` 33%, rgba(0, 0, 0, 0) 33%), linear-gradient(160deg, ` + backColor2 + ` 66%, ` + backColor3 + ` 66%)`
+  };
+  return (
+    <div 
+        className={['card', mode].join(' ')} 
+        style={cardBackground}
+        {...props}
+        >
+    </div>
+
+ //   <button
+   //   type="button"
+ //     className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+ //     style={backgroundColor && { backgroundColor }}
+ //     {...props}
+ //   >
+ //     {label}
+ //   </button>
+  );
+};
+
+Card.propTypes = {
+  /**
+   * Is the card dealt? If so, show the front, otherwise, show the back. 
+   */
+  dealt: PropTypes.bool,
+  /**
+   * What background colors to use
+   */
+  backColor1: PropTypes.string,
+  backColor2: PropTypes.string,
+  backColor3: PropTypes.string,
+  frontColor: PropTypes.string,
+  /**
+   * How large should the button be?
+   */
+  //size: PropTypes.oneOf(['small', 'medium', 'large']),
+  /**
+   * Button contents
+   */
+  //label: PropTypes.string.isRequired,
+  /**
+   * Optional click handler
+   */
+  onClick: PropTypes.func,
+};
+
+Card.defaultProps = {
+   dealt: false,
+  onClick: undefined,
+  backColor1: "#e07a35", 
+    backColor2: "#b1cb55",
+    backColor3: "#6e3288",
+    frontColor: "#b1cb55",
+};
--- stories/Card.stories.jsx
+++ stories/Card.stories.jsx
@@ -1,32 +1,40 @@
 import React from 'react';
 
-import { Button } from './Button';
+import { Card } from './Card';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'Example/Button',
-  component: Button,
+  title: 'Game/Card',
+  component: Card,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
-    backgroundColor: { control: 'color' },
+    backColor1: { control: 'color' },
+    backColor2: { control: 'color' },
+    backColor3: { control: 'color' },
+    frontColor: { control: 'color' },
   },
 };
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template = (args) => <Button {...args} />;
+const Template = (args) => <Card {...args} />;
 
-export const Primary = Template.bind({});
+export const Dealt = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
-Primary.args = {
-  primary: true,
-  label: 'Aaron',
+Dealt.args = {
+    backColor1: "#e07a35", 
+    backColor2: "#b1cb55",
+    backColor3: "#6e3288",
+    frontColor: "#b1cb55",
 };
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: 'Button',
+export const inDeck = Template.bind({});
+inDeck.args = {
+    backColor1: "#e07a35", 
+    backColor2: "#b1cb55",
+    backColor3: "#6e3288",
+    frontColor: "#b1cb55",
 };
-
+/*
 export const Large = Template.bind({});
 Large.args = {
   size: 'large',
@@ -38,3 +46,4 @@ Small.args = {
   size: 'small',
   label: 'Button',
 };
+ */
